548f27343bc5eeba1ac36ce6285bb3717ae9450f
Jeff Kelly <acidburner22@hotmail.com>
2017-02-07 21:50:27 -0800

    version 0.13
    added buttons
    made function names more appropriate.
    fixed scoping issues cause javascript and anonymous functions.
    blocks render to screen.
    todo: dropPiece and redrawing

--- TetrisJS.js
+++ TetrisJS.js
@@ -106,11 +106,11 @@ class Grid {
         console.log("creating Grid class");
         this.NUM_COLS = 15;
         this.NUM_ROWS = 30;
-        this.map = this.makeGrid( this.NUM_ROWS, this.NUM_COLS );
-        this.moveGrid = this.makeGrid( this.NUM_ROWS, this.NUM_COLS );
+        this.map = this.createGrid( this.NUM_ROWS, this.NUM_COLS );
+        //this.moveGrid = this.createGrid( this.NUM_ROWS, this.NUM_COLS );
     }
 
-    makeGrid( NUM_ROWS, NUM_COLS ){
+    createGrid( NUM_ROWS, NUM_COLS ){
         console.log("making grid");
         var grid = new Array( NUM_ROWS );
         for( var i = 0; i < NUM_ROWS; i++ ) {
@@ -124,32 +124,28 @@ class Grid {
         return grid;
     }
 
-    clearGrid( grid ) {
+    clearGrid( ) {
         for( var y = 0; y < this.NUM_COLS; y++ ) {
             for( var x = 0; x < this.NUM_ROWS; x++ ) {
-                grid[y][x] = 0;
+                this.map[y][x] = 0;
             }
         }
-        return grid;
     }
 
-    addPieceToGrid( grid, piece ) {
-        this.clearMoveGrid();
+    addPieceToGrid( piece ) {
         for( var i = 0; i < 4; i++ ) {
-            for( var k = 0; l < 4; k++ ) {
-                grid[piece.location.y+i][piece.location.x+k] = piece.block[i][k];
+            for( var k = 0; k < 4; k++ ) {
+                this.map[piece.location.y+i][piece.location.x+k] = piece.block.mapping[i][k];
             }
         }
-        return grid;
     }
 
-    static updateGrid( ) {
-        for( var y = 0; y < this.NUM_COLS; y++ ) {
-            for( var x = 0; x < this.NUM_ROWS; x++ ) {
-                this.map[y][x] = this.moveGrid[y][x];
-            }
-            
-        }
+    updateGrid( ) {
+        // for( var y = 0; y < this.NUM_COLS; y++ ) {
+        //     for( var x = 0; x < this.NUM_ROWS; x++ ) {
+        //         //this.map[y][x] = this.moveGrid[y][x];
+        //     }
+        // }
     }
 }
 
@@ -167,7 +163,7 @@ class Render {
     }
 
     //draw tile at x and y
-    drawTile( x, y, color ) {
+    renderTile( x, y, color ) {
         this.canvasContext.fillStyle = color;
         this.canvasContext.fillRect( x * this.TILE_SIZE, y * this.TILE_SIZE, this.TILE_SIZE, this.TILE_SIZE );
     }
@@ -204,35 +200,35 @@ class Render {
         }
     }
 
-    drawGrid( grid, piece ) {
+    renderGrid( grid, piece ) {
         for( var y = 0; y < grid.NUM_COLS; y++ ) {
             for( var x = 0; x < grid.NUM_ROWS; x++ ) {
                 if( grid.map[y][x] === "X" ) {
-                    this.drawTile( x, y, piece.block.color );
+                    this.renderTile( x, y, piece.block.color );
                 }
                 if( grid.map[y][x] === 1 ) {
-                    this.drawTile( x, y, piece.block.color );
+                    this.renderTile( x, y, piece.block.color );
                 }
             }
         }
     }
 
     //draws shape at x,y offset by y+i,x+k of shape
-    drawShape( shape, x, y ) {
+    renderShape( shape, x, y ) {
         for( var i = 0; i < shape.mapping.length; i++ ) {
             for( var k = 0; k < shape.mapping.length; k++ ) {
                 if( shape.mapping[i][k] === "X" ) {
-                    this.drawTile( x + k, y + i, shape.color );
+                    this.renderTile( x + k, y + i, shape.color );
                 }
             }
         }
     }
 
     //randomly fill screen with shapes to see them -- testing function
-    drawAllShapes( ) {
+    renderAllShapes( ) {
         for( var i = 0; i < 6; i++ ){ 
             for(var k = 0; k < 3; k++ ) {
-                this.drawShape( Piece.getRandom(), k*5, i*5 );
+                this.renderShape( Piece.getRandom(), k*5, i*5 );
             }
         }
     }
@@ -245,6 +241,7 @@ class Points {
 
 class TetrisJS {
     constructor() {
+        var self = this;
         console.log("creating new tetris game");
         this.PLACED_TILE = 2;
         this.FRAMES_PER_SECOND = 1;
@@ -262,20 +259,29 @@ class TetrisJS {
             "block": Piece.getRandom()
         }
         window.addEventListener( "keypress", this.handleKeypress );
-        //setInterval( this.updateLoop, this.UPDATE_FREQUENCY / this.FRAMES_PER_SECOND );
+        document.getElementById( "new" ).addEventListener( "click", function( ){
+            self.newGame();
+        }, false );
+        document.getElementById( "pause" ).addEventListener( "click", function( ){
+            self.pauseGame();
+        }, false );
+        document.getElementById( "resume" ).addEventListener( "click", function( ){
+            self.resumeGame();
+        }, false );
     }
 
-    updateLoop(){
+    updateLoop( ){
+        console.log("update loop");
         //move piece down 1 unit
-        //this.dropPiece();
+        this.dropPiece();
         //check move
             //check hit detection, if( [i++][k] == 1 ), stop piece, else continue
             //check for valid move/game over
         //update screen
-        //this.render.drawGrid( this.grid, this.piece );
+        this.render.renderGrid( this.grid, this.piece );
         //update score
         //if piece can't drop vertically, get new piece
-        //this.getNewPiece();
+        //this.createNewPiece();
     }
 
     
@@ -283,6 +289,7 @@ class TetrisJS {
     createNewPiece() {
         this.piece.location = this.START_LOCATION;
         this.piece.block = Piece.getRandom();
+        this.grid.addPieceToGrid( this.piece );
     }
 
     dropPiece() {
@@ -325,5 +332,35 @@ class TetrisJS {
                 console.log("right");
                 break;   
         }
+    }
+
+    previewPiece() {
+        //stub
+        console.log( "creating preview piece");
+    }
+
+    newGame() {
+        var self = this;
+        console.log("starting new game");
+        this.grid.clearGrid();
+        this.render.clearAllTiles( this.grid );
+        this.createNewPiece();
+        
+        this._timer = setInterval( function(){ 
+            self.updateLoop() 
+        }, this.UPDATE_FREQUENCY / this.FRAMES_PER_SECOND );
+    }
+
+    pauseGame() {
+        console.log("pausing game");
+        clearInterval( this._timer );
+    }
+
+    resumeGame() {
+        var self = this;
+        console.log("resuming game");
+        this._timer = setInterval( function(){ 
+            self.updateLoop() 
+        }, this.UPDATE_FREQUENCY / this.FRAMES_PER_SECOND );
     }
 }
